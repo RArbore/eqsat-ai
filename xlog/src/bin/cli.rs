327008c1f0d6a39cbb8090433709675fad018f28
Russel Arbore <russel.jma@gmail.com>
2025-09-20 13:32:31 -0700

    Simple cli interface

---
+++ xlog/src/bin/cli.rs
@@ -0,0 +1,30 @@
+use std::io::{Result, stdin};
+
+use ds::uf::UnionFind;
+
+use xlog::database::{Database, DatabaseAuxiliaryState};
+use xlog::fixpoint::fixpoint;
+use xlog::frontend::Interner;
+use xlog::grammar::ProgramParser;
+
+pub fn main() -> Result<()> {
+    let uf = UnionFind::new();
+    let mut interner = Interner::new();
+    let aux_state = DatabaseAuxiliaryState { uf: &uf };
+    let mut database = Database::new(aux_state);
+    let mut program = vec![];
+    for line in stdin().lines() {
+        let mut line = line?;
+        if !line.ends_with(";") {
+            line += ";";
+        }
+        let line = ProgramParser::new().parse(&mut interner, &mut database, &line);
+        match line {
+            Ok(rules) => program.extend(rules),
+            Err(err) => println!("{}", err),
+        }
+    }
+    fixpoint(&mut database, &program);
+    println!("{:?}", database);
+    Ok(())
+}
