f600a7c708b5791fed57c60f7bbded732ff686d2
Russel Arbore <russel.jma@gmail.com>
2025-12-02 12:06:14 -0800

    restart

--- imp/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use lalrpop_util::lalrpop_mod;
-
-pub mod ai;
-pub mod ast;
-pub mod lattice;
-
-lalrpop_mod!(pub grammar);
--- xlog/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use lalrpop_util::lalrpop_mod;
-
-pub mod action;
-pub mod database;
-pub mod fixpoint;
-pub mod frontend;
-pub mod query;
-
-lalrpop_mod!(pub grammar);
